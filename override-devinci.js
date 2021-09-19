6738cc1f1bb9e85de58a7b12befba487676cef37
Legeron <quentin.legeron@gmail.com>
2021-09-19 20:41:00 +0200

    :bulb: add comment to accordion beacause it's broken

--- override-devinci.js
+++ override-devinci.js
@@ -1,12 +1,20 @@
-document.getElementsByTagName('body')[0].classList.add('override')
-document.querySelectorAll('header').classList.add('override2')
+document.getElementsByTagName('body')[0].classList.add('override');
+document.querySelectorAll('header').classList.add('override2');
 
 //Accordieon in information coronavirus
+/*I try to find the closest div to where I will put my accordion
+And then I change 
+*/
 var finder = document.getElementsByClassName("pull-right text-right");
-var accordionCell = finder.nextElementSibling;
+var noNameDiv = finder.nextElementSibling;
+var accordionCell = noNameDiv.nextElementSibling;
+accordionCell = document.createElement("div");
+accordionCell.className = "accordion-cell-home-page";
 var i;
 
 //creation of the toggle for the accordion
+/*I want to create a button 
+*/
 var acc = document.createElement("button");
 acc.className = "accordion";
 var text = document.createTextNode("Voir plus ...");
@@ -14,6 +22,7 @@ acc.appendChild(text);
 accordionCell.appendChild(acc);
 
 // fonction on listener 
+/*When the button is created, I want the following text to be displayed or not on a click. */
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
       /* Toggle between adding and removing the "active" class,
